6492fea1f6f1de6bc535da7f8013c47e16db63d6
DESKTOP-I7MS7HN\Antrov <antrof@gmail.com>
2017-07-11 22:02:56 +0200

    Segger added

--- controller.c
+++ controller.c
@@ -5,6 +5,7 @@
 #include "nrf_gpio.h"
 #include "controller.h"
 #include "boards.h"
+#include "SEGGER_RTT.h"
 
 #define GPIO_SWITCH_INPUT								25
 #define GPIO_SWITCH_CONTROL							28
@@ -12,19 +13,39 @@
 #define GPIO_MOTOR_DOWN									15
 #define GPIO_TICK_INPUT 								29
 
+int position = 0;
+int movement = MOVE_DIRECTION_NONE;
+
 static bool isInit = false; 
 
+void emergency_break() 
+{
+		nrf_gpio_pin_clear(GPIO_MOTOR_DOWN);
+		nrf_gpio_pin_clear(GPIO_MOTOR_UP);
+}
 
 void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
-    nrf_gpio_pin_toggle(GPIO_MOTOR_UP);
+		switch (movement) {
+				case MOVE_DIRECTION_DOWN:
+						position--;
+						break;
+				case MOVE_DIRECTION_UP:
+						position++;
+						break;
+				case MOVE_DIRECTION_NONE:
+						emergency_break();
+						break;
+				default: break;
+		}
+    //nrf_gpio_pin_toggle(GPIO_MOTOR_UP);
 }
 
 void switch_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
-    nrf_gpio_pin_toggle(GPIO_MOTOR_DOWN);
+		SEGGER_RTT_printf(0, "Siemka"); // Print service UUID should match definition BLE_UUID_OUR_SERVICE
+    nrf_gpio_pin_toggle(GPIO_SWITCH_CONTROL);
 }
-
 
 void controller_init(void)
 {
--- controller.h
+++ controller.h
@@ -3,6 +3,7 @@
 
 #define MOVE_DIRECTION_UP			0xCB
 #define MOVE_DIRECTION_DOWN		0x92
+#define MOVE_DIRECTION_NONE		0x00
 
 void controller_init(void);
 void controller_move(uint8_t direction);
