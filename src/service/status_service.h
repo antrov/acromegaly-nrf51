f0537b236408c07abd5dece326f41b4fa8fac85f
Hubert Andrzejewski <antrof@gmail.com>
2018-08-23 20:15:38 +0200

    Improved services UUID; README updated;

--- src/service/status_service.h
+++ src/service/status_service.h
@@ -1,18 +1,18 @@
 #ifndef STATUS_SERVICE_H__
 #define STATUS_SERVICE_H__
 
-#include <stdint.h>
 #include "ble.h"
 #include "ble_srv_common.h"
+#include <stdint.h>
 
 #define BLE_UUID_STATUS_BASE_UUID                                                                          \
-	{                                                                                                      \
-		{                                                                                                  \
-			0x23, 0xD1, 0x13, 0xEF, 0x5F, 0x78, 0x23, 0x15, 0xDE, 0xEF, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00 \
-		}                                                                                                  \
-	}								   // 128-bit base UUID
-#define BLE_UUID_STATUS_SERVICE 0xABCD // Just a random, but recognizable value
-#define BLE_UUID_STATUS_CHARACTERISTC_UUID 0xBEEF
+    {                                                                                                      \
+        {                                                                                                  \
+            0x23, 0xD1, 0x13, 0xEF, 0x5F, 0x78, 0x23, 0x15, 0xDE, 0xEF, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00 \
+        }                                                                                                  \
+    }
+#define BLE_UUID_STATUS_SERVICE 0x5E1F
+#define BLE_UUID_STATUS_CHARACTERISTC_UUID 0xFEED
 
 /**
  * @brief This structure contains various status information for our service. 
@@ -23,19 +23,19 @@
  */
 typedef struct
 {
-	uint16_t conn_handle;
-	uint16_t service_handle;
-	ble_gatts_char_handles_t char_handles;
+    uint16_t conn_handle;
+    uint16_t service_handle;
+    ble_gatts_char_handles_t char_handles;
 } ble_ss_t;
 
-void ble_status_service_on_ble_evt(ble_ss_t *p_status_service, ble_evt_t *p_ble_evt);
+void ble_status_service_on_ble_evt(ble_ss_t* p_status_service, ble_evt_t* p_ble_evt);
 
 /**@brief Function for initializing our new service.
  *
  * @param[in]   p_our_service       Pointer to Our Service structure.
  */
-void status_service_init(ble_ss_t *p_status_service);
+void status_service_init(ble_ss_t* p_status_service);
 
-void status_characteristic_update(ble_ss_t *p_status_service, int16_t pos, int16_t target, uint8_t mov, uint8_t sw);
+void status_characteristic_update(ble_ss_t* p_status_service, int16_t pos, int16_t target, uint8_t mov, uint8_t sw);
 
 #endif /* _ OUR_SERVICE_H__ */
